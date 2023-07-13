a37c46fe73b3c8d0f729c9ecb9fa3cc831d7ecd5
hotolla <86011142+hotolla@users.noreply.github.com>
2023-07-13 14:06:02 +0200

    added NEXT_PUBLIC_FREE_CURRENCY_API_KEY

--- src/api/exchangeRates.ts
+++ src/api/exchangeRates.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from "axios";
 
 export const fetchExchangeRates = (config?: AxiosRequestConfig) => {
   return axios.get(
-    `https://api.freecurrencyapi.com/v1/latest?apikey=u4fOT74Ev0O5E3p2LGKWNQF5cy6CTnKykEG4QaZR`,
+    `https://api.freecurrencyapi.com/v1/latest?apikey=${process.env.NEXT_PUBLIC_FREE_CURRENCY_API_KEY}`,
     config
   ).then(({ data}) => {
     return data;
@@ -21,4 +21,10 @@ export const fetchExchangeRates = (config?: AxiosRequestConfig) => {
 // export const fetchExchangeRates = () => {
 //   return fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=u4fOT74Ev0O5E3p2LGKWNQF5cy6CTnKykEG4QaZR`)
 //     .then((response) => response.json());
+// };
+
+// export const fetchExchangeRates = (config?: AxiosRequestConfig) => {
+//   return fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=u4fOT74Ev0O5E3p2LGKWNQF5cy6CTnKykEG4QaZR&`
+//     + new URLSearchParams(config?.params)
+//     ).then((response) => response.json());
 // };
